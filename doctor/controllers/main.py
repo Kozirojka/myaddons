92b402d5f5432d1c837807aaba7d22ef87e92475
Stalker <vihovaneckostik@gmil.com>
2025-06-02 20:54:29 +0300

    doctor url

--- doctor/controllers/main.py
+++ doctor/controllers/main.py
@@ -2,6 +2,6 @@ from odoo import http
 from odoo.http import request
 
 class DoctorWebsite(http.Controller):
-    @http.route(['/doctor/patient/treatment'], type='http', auth="public", website=True)
+    @http.route(['/doctor/patient/<int:id>/treatment'], type='http', auth="public", website=True)
     def doctor_page(self, **kw):
         return request.render("doctor.doctor_page")
