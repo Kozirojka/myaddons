fb641d129433340c45a198d8b9ca9b542eda6d0c
Stalker <vihovaneckostik@gmil.com>
2025-06-03 18:52:48 +0300

    workable tasks

--- doctor/static/src/components/tabs/tasks/tasks.js
+++ doctor/static/src/components/tabs/tasks/tasks.js
@@ -2,6 +2,7 @@ import { Component, useState, onWillStart } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Task } from "./task/task";
 import tasksData from "./tasks_data";
+import { rpc } from "@web/core/network/rpc";
 
 export class Tasks extends Component {
   static template = "doctor.Tasks";
@@ -16,7 +17,42 @@ export class Tasks extends Component {
       loading: false,
       error: null,
     });
+
+    this.loadTasks();
   }
+
+   getPatientIdFromUrl() {
+        // Отримуємо ID з URL: /doctor/patient/123/treatment
+        const path = window.location.pathname;
+        const match = path.match(/\/doctor\/patient\/(\d+)\/treatment/);
+        return match ? parseInt(match[1]) : null;
+    }   
+
+    async loadTasks() {
+
+        try {
+            console.log("Loading tasks...");
+            
+            const patientId = this.getPatientIdFromUrl();
+
+
+            console.log(`Patient ID: ${patientId}`);
+
+
+            const result = await rpc(`/doctor/patient/data/${patientId}/tasks`);
+
+            console.log(result);
+
+            this.state.tasks = result.exercise_cases;
+            
+            console.log("TASKS tasks successfully");
+
+        } catch (error) {
+            this.state.error = error.message;
+        } finally {
+            this.state.isLoading = false;
+        }
+    }
   
 }
 
